bc01a3b5f078b24cf2f26362f4eb4bfa1255249d
Jeroen Bellen <bellenjeroen@gmail.com>
2014-10-15 20:15:52 +0200

    Improve source formatting and improve formatting

--- src/test/java/com/github/jeroenbellen/aedifex/generation/ClassGeneratorTest.java
+++ src/test/java/com/github/jeroenbellen/aedifex/generation/ClassGeneratorTest.java
@@ -9,7 +9,7 @@ import static org.fest.assertions.Assertions.assertThat;
 public class ClassGeneratorTest {
 
     @Test
-    public void generateClass() throws Exception {
+    public void generateClassWithOneField() throws Exception {
         final ArrayList<FieldProperty> fields = new ArrayList<FieldProperty>();
         fields.add(FieldProperty.of("bar", "java.lang.bar"));
         final ClassProperties classProperties = ClassProperties.of("Foo", "bar", fields);
@@ -40,9 +40,64 @@ public class ClassGeneratorTest {
                         "    public $Foo bar(java.lang.bar bar) {\n" +
                         "        new Mirror().on(inst).set().field(\"bar\").withValue(bar);\n" +
                         "        return this;\n" +
-                        "    }\n" +
+                        "    }\n\n" +
                         "}");
 
     }
 
+    @Test
+    public void generateClassWithTwoFields() throws Exception {
+        final ArrayList<FieldProperty> fields = new ArrayList<FieldProperty>();
+        fields.add(FieldProperty.of("bar", "java.lang.bar"));
+        fields.add(FieldProperty.of("aze", "java.lang.bar"));
+        final ClassProperties classProperties = ClassProperties.of("Foo", "bar", fields);
+        final String generatedClass = ClassGenerator.generate(classProperties);
+        assertThat(generatedClass)
+                .isEqualTo("package bar;\n" +
+                        "\n" +
+                        "import net.vidageek.mirror.dsl.Mirror;\n" +
+                        "import javax.annotation.Generated;\n" +
+                        "\n" +
+                        "@Generated(\"com.github.jeroenbellen.aedifex.processor.AedifexProcessor\")\n" +
+                        "public final class $Foo {\n" +
+                        "\n" +
+                        "    private final Foo inst;\n" +
+                        "\n" +
+                        "    private $Foo(){\n" +
+                        "        inst = new Mirror().on(Foo.class).invoke().constructor().withoutArgs();\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public static $Foo with() {\n" +
+                        "        return new $Foo();\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public Foo build() {\n" +
+                        "        return inst;\n" +
+                        "    }\n" +
+                        "\n" +
+                        "    public $Foo bar(java.lang.bar bar) {\n" +
+                        "        new Mirror().on(inst).set().field(\"bar\").withValue(bar);\n" +
+                        "        return this;\n" +
+                        "    }\n" +
+                        "    public $Foo aze(java.lang.bar aze) {\n" +
+                        "        new Mirror().on(inst).set().field(\"aze\").withValue(aze);\n" +
+                        "        return this;\n" +
+                        "    }\n\n" +
+                        "}");
+
+    }
+
+    @Test
+    public void generateClassWhenPackageIsNull() throws Exception {
+        final ClassProperties classProperties = ClassProperties.of("Foo", null, new ArrayList<FieldProperty>());
+        final String generatedClass = ClassGenerator.generate(classProperties);
+        assertThat(generatedClass).doesNotContain("package");
+    }
+
+    @Test
+    public void generateClassWhenPackageIsEmpty() throws Exception {
+        final ClassProperties classProperties = ClassProperties.of("Foo", "", new ArrayList<FieldProperty>());
+        final String generatedClass = ClassGenerator.generate(classProperties);
+        assertThat(generatedClass).doesNotContain("package");
+    }
 }
